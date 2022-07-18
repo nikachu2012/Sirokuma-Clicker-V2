a4b5dc9e33e034add8afcd9969e915d0a5bd3e6e
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-07-18 11:25:51 +0900

    [delete] 使われていないので削除

--- src/assets/javascript/error.js
+++
@@ -1,4 +0,0 @@
-function error_critical(errorcode){
-    alert("Sirokuma-Clicker Version" + clicker_version + " Bulid " +  clicker_bulid +" \ncritical error.  Error Code" + errorcode)
-    window.location.reload();
-}
