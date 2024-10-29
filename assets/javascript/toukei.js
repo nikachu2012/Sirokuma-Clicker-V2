4cf7246c266fa19d09b5f53ae454a529bea6b518
sirokuma2020 <sirotakumadayo2@gmail.com>
2024-10-29 17:36:28 +0900

    ひとつ前と同じ

--- assets/javascript/toukei.js
+++ assets/javascript/toukei.js
@@ -3,7 +3,7 @@
 const refreshToukei = ()=>{
     let keika_jikan = []
     let now_Date = Math.floor(Date.now() / 1000)
-    keika_jikan[10] = Math.floor(now_Date-start_date)
+    keika_jikan[10] = Math.floor(now_Date-startDate)
     keika_jikan[0] = Math.floor(keika_jikan[10] / 31536000)
     keika_jikan[1] = Math.floor(keika_jikan[10] % 31536000 / 86400)
     keika_jikan[10] = keika_jikan[10] % 31536000 % 86400
